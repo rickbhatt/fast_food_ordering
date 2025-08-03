d79997d58b388542d137d503535a1b59ce3ef39f
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2025-08-03 13:10:14 +0530

    homepage UI completed

--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,9 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+config.resolver.alias = {
+  "@": "./src",
+  "@assets": "./assets",
+};
 
 module.exports = withNativeWind(config, { input: "./src/app/globals.css" });
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,32 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 import "./globals.css";
 
+SplashScreen.preventAutoHideAsync();
+
+const InitialLayout = () => {
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Bold": require("../../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Medium": require("../../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-Regular": require("../../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-SemiBold": require("../../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Light": require("../../assets/fonts/Quicksand-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (error) {
+      console.log("fonts error", error);
+      throw error;
+    }
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, error]);
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
+
 export default function RootLayout() {
-  return <Stack />;
+  return <InitialLayout />;
 }
