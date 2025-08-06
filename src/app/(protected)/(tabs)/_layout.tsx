b07dbd3b539c0852f1768c29b7c8b299da838080
Ritankar Bhattacharjee <ritankar.bhattacharjee80@gmail.com>
2025-08-06 13:55:19 +0530

    added custom tab bar navigation

--- src/app/(protected)/(tabs)/_layout.tsx
+++ src/app/(protected)/(tabs)/_layout.tsx
@@ -1,8 +1,102 @@
+import { images } from "@/constants";
+import cn from "clsx";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { TabBarIconProps } from "type";
+
+const TabBarIcon = ({ focused, icon, title }: TabBarIconProps) => (
+  <View className="tab-icon">
+    <Image
+      source={icon}
+      className="size-7"
+      resizeMode="contain"
+      tintColor={focused ? "#fe8c00" : "#5d5f6d"}
+    />
+    <Text
+      className={cn(
+        "text-sm font-bold",
+        focused ? "text-primary" : "text-gray-200"
+      )}
+    >
+      {title}
+    </Text>
+  </View>
+);
 
 const TabsLayout = () => {
-  return <Tabs screenOptions={{ headerShown: false }} />;
+  const { bottom } = useSafeAreaInsets();
+
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          borderBottomLeftRadius: 50,
+          borderBottomRightRadius: 50,
+          marginHorizontal: 20,
+          height: 80,
+          position: "absolute",
+          bottom: bottom,
+          backgroundColor: "white",
+          shadowColor: "#1A1A1A",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          elevation: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} title="Home" icon={images.home} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} title="Search" icon={images.search} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} title="Cart" icon={images.bag} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              focused={focused}
+              title="Profile"
+              icon={images.person}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 };
 
 export default TabsLayout;
